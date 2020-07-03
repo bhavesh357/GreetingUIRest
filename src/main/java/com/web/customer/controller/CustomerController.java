66403f13ba4ce60fca2c5908b03b0b5d0e9b0920
bhavesh357 <bkadam357@gmail.com>
2020-07-03 18:56:44 +0530

    REFACTORED: dto and controller to validate

--- src/main/java/com/web/customer/controller/CustomerController.java
+++ src/main/java/com/web/customer/controller/CustomerController.java
@@ -41,4 +41,5 @@ public class CustomerController {
         service.deleteCustomer(id);
     }
 
+
 }
--- src/main/java/com/web/customer/dto/CustomerDto.java
+++ src/main/java/com/web/customer/dto/CustomerDto.java
@@ -1,12 +1,16 @@
 package com.web.customer.dto;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 public class CustomerDto {
-    @Pattern(regexp = "[A-Z][a-z]{2,12}")
+
+    @NotNull(message = "Enter a Name with Capital Letter and at least 2 more characters")
+    @Pattern(regexp = "[A-Z][a-z]{2,12}",message = "Enter a Name with Capital Letter and at least 2 more characters")
     private String firstName;
 
-    @Pattern(regexp = "[A-Z][a-z]{2,12}")
+    @NotNull(message = "Enter a Name with Capital Letter and at least 2 more characters")
+    @Pattern(regexp = "[A-Z][a-z]{2,12}",message = "Enter a Name with Capital Letter and at least 2 more characters")
     private String lastName;
 
     public CustomerDto() {
