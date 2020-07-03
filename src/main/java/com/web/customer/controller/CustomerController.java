817b0d63dd78491670edeb85a3a626f363110064
bhavesh357 <bkadam357@gmail.com>
2020-07-03 18:59:49 +0530

    REFACTORED: exception handling to handle bad request

--- src/main/java/com/web/customer/controller/CustomerController.java
+++ src/main/java/com/web/customer/controller/CustomerController.java
@@ -4,10 +4,15 @@ import com.web.customer.dto.CustomerDto;
 import com.web.customer.model.Customer;
 import com.web.customer.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
@@ -41,5 +46,16 @@ public class CustomerController {
         service.deleteCustomer(id);
     }
 
-
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
 }
